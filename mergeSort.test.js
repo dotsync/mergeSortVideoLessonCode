ceb12a1afd6014637c98e6cdfdf4b5d96e2a8650
Sergei Liddell <liddell.s@pm.me>
2021-05-11 17:40:37 -0600

    Creates test suite for mergeSort.mergeSortRecursiv

--- mergeSort.test.js
+++ mergeSort.test.js
@@ -30,6 +30,13 @@ describe('MERGE SORT ALGORITHM', () => {
     });
   });
   describe('mergeSortRecursive function', () => {
-
+    test('should return an empty array if given an empty array as input', () => {
+      expect(mergeSort.mergeSortRecursive([])).toStrictEqual([]);
+    });
+    test('should should sort the input array in ascending order', () => {
+      const actual = mergeSort.mergeSortRecursive([1, 9, 8, 3, 6, 2, 5, 10, 4, 7]);
+      const expected = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+      expect(actual).toStrictEqual(expected);
+    });
   });
 });
